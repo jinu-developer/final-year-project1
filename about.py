29e710f34d8656c5287ba4c6ab291830111fb428
Harshit Sharma <91822610+Hs918131@users.noreply.github.com>
2023-10-10 22:16:41 +0530

    Update about.py

--- about.py
+++ about.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import streamlit-extras
+from streamlit-extras
 
 from streamlit_extras.switch_page_button import switch_page
 st.set_page_config(page_title="OncoMRI: TumorTeller", page_icon="💊", layout="wide")
